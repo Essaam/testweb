eb599c48d10aedc118936b61d04c20bcb686a051
Hessam <hesamnik@gmail.com>
2016-08-18 00:07:50 +0200

    Puts logo in Nav bar and fixes JS for Nav bar collapse and text replacement.

--- scripts/myjs.js
+++ scripts/myjs.js
@@ -1,4 +1,9 @@
-﻿window.onload(replaceText('HOME'));
+﻿function collapseNavBar() {
+    nv = document.getElementById('bs-navbar-collapse');
+    nv.classList.remove('in');
+    nv.setAttribute('aria-expanded', 'false');
+}
+
 function replaceText(idname) {
     mt = document.getElementById('maintext');
     txtarget = document.getElementById(idname);    
@@ -7,4 +12,11 @@ function replaceText(idname) {
     for (kk = 0; kk < navEL.childElementCount; kk++) { navEL.children[kk].className = ""; }
     targetnav = document.getElementById('nav' + idname);
     targetnav.className = "active";
+
+    collapseNavBar();
 }
+
+document.addEventListener("DOMContentLoaded", function () { replaceText('HOME') });
+document.getElementById('navHOME').addEventListener("click", function () { replaceText('HOME') });
+document.getElementById('navABOUT').addEventListener("click", function () { replaceText('ABOUT') });
+document.getElementById('navCONTACT').addEventListener("click", function () { replaceText('CONTACT') });
