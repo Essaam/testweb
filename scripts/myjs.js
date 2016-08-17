37d5002019553538dcb236e68f4a59f56756c93f
Hessam <hesamnik@gmail.com>
2016-08-17 13:53:53 +0200

    Added all

---
+++ scripts/myjs.js
@@ -0,0 +1,10 @@
+﻿window.onload(replaceText('HOME'));
+function replaceText(idname) {
+    mt = document.getElementById('maintext');
+    txtarget = document.getElementById(idname);    
+    mt.innerHTML = txtarget.innerHTML;    
+    navEL = document.getElementById('navEl');
+    for (kk = 0; kk < navEL.childElementCount; kk++) { navEL.children[kk].className = ""; }
+    targetnav = document.getElementById('nav' + idname);
+    targetnav.className = "active";
+}
